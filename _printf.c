55c69372a592be7434f51645db07ac84512c28b7
Princengwane <hlongz920@gmail.com>
2022-11-03 02:54:46 -0700

    Printf

--- _printf.c
+++ _printf.c
@@ -37,7 +37,7 @@ int _printf(const char *format, ...)
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer;
+			printed = handle_print(format, &i, list, buffer,
 					flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -47,4 +47,14 @@ int _printf(const char *format, ...)
 	print_buffer(buffer, &buff_ind);
 	va_end(list);
 	return (printed_chars);
+}
+/**
+ *  @buffer: Array of chars
+ *  @buff_ind: Index at which to add next char, represents the length
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
 }
